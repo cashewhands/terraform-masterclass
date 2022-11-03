f4d16f7dbafebd27cc3e4d781dce3480899d5424
Demola <demoladev.ops@gmail.com>
2022-11-03 20:56:24 +0000

    correction made

--- vpc.tf
+++ vpc.tf
@@ -2,8 +2,8 @@
 
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  /* enable_dns_support   = true
+  enable_dns_hostnames = true */
 }
 
 resource "aws_flow_log" "vpc-fl" {
